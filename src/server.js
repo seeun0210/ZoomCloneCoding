c074120f2239214652a48da346543c19e47c1402
KimSeEun <cocobell3@naver.com>
2023-10-22 08:17:57 +0900

    feat: ws, http 둘 다 인식할 수 있는 서버 만들기

--- src/server.js
+++ src/server.js
@@ -1,18 +1,24 @@
 import exp from "constants";
 import express from "express";
+import http from "http";
+import { WebSocketServer } from "ws";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const app = express();
-app.set("view engine", "pug");
 
+app.set("view engine", "pug");
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log("Listening to http://localhost:3000");
-app.listen(3000, handleListen);
+const handleListen = () => console.log("Listening on http://localhost:3000");
+//express는 ws를 따로 지원하지 않기 때문에 function을 만들어 주어야 한다
+//이렇게 해서 서버는 http와 ws 두개의 protocol을 이해할 수 있게 됨
+//항상 이렇게 할 필요는 없고 websocet server만 만들어도 된다.
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+server.listen(3000, handleListen);
